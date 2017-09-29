76996f1b0b8795434c695194e25e7bd3f36dd7f7
Gabriel Florit <gabrielflorit@gmail.com>
2017-09-29 17:08:12 -0400

    Write results to csv

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # google-geocoder-cli
+
+TODO
+- add a progress bar
--- bin/google-geocoder-cli.js
+++ bin/google-geocoder-cli.js
@@ -4,6 +4,7 @@ const dsv = require('d3-dsv')
 const fs = require('fs')
 const NeDB = require('nedb')
 const PromisePool = require('es6-promise-pool')
+const _ = require('lodash')
 
 const argv = require('yargs')
   .usage('Geocode a CSV of addresses, using Google Maps API.')
@@ -28,7 +29,10 @@ const db = new NeDB({ filename: 'geocode.nedb', autoload: true })
 // Read CSV.
 const rows = dsv.csvParse(fs.readFileSync(argv.f, 'utf8'))
 
+const geocodedRows = []
+
 // Geocode a row. Returns a Promise.
+let geocodeRequests = 0
 const geocodeRow = function (row) {
   return new Promise(function (resolve, reject) {
     const { address } = row
@@ -40,17 +44,17 @@ const geocodeRow = function (row) {
         reject(err)
       } else if (docs.length) {
         // otherwise, if there is a match, resolve the promise;
-        resolve()
+        resolve({ row, geocode: docs[0].response })
       } else {
         // otherwise, geocode.
 
         googleMapsClient.geocode({ address }, function (err, response) {
-          console.log(`Geocoded ${address}`)
+          geocodeRequests++
           if (err) {
             reject(err)
           } else {
             db.insert({ address, response })
-            resolve()
+            resolve({ row, geocode: response })
           }
         })
       }
@@ -74,6 +78,20 @@ const concurrency = 1
 // Create the promise pool,
 const pool = new PromisePool(promiseProducer, concurrency)
 
+// listen to fulfilled,
+pool.addEventListener('fulfilled', e => {
+  const { row } = e.data.result
+  const coords = _.get(
+    e,
+    'data.result.geocode.json.results[0].geometry.location'
+  )
+
+  geocodedRows.push({
+    ...row,
+    geocode: _.values(coords).join(',')
+  })
+})
+
 // listen to rejection,
 pool.addEventListener('rejected', e => {
   console.log(JSON.stringify(e.data.error, null, 2))
@@ -83,7 +101,10 @@ pool.addEventListener('rejected', e => {
 pool
   .start()
   .then(() => {
-    console.log('all done')
+    const filename = `geocoded-${argv.f}`
+    fs.writeFileSync(filename, JSON.stringify(geocodedRows, null, 2))
+    console.log(`Wrote ${geocodedRows.length} records to ${filename}.`)
+    console.log(`Hit Google ${geocodeRequests} times.`)
   })
   .catch(e => {
     console.error(e)
